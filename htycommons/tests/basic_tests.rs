65fb32e68d9c4f0adfd59b8d938e5e4c7428b1c1
阿男 <l.weinan@gmail.com>
2025-08-07 23:38:16 +0800

    save progress

--- htycommons/tests/basic_tests.rs
+++ htycommons/tests/basic_tests.rs
@@ -41,28 +41,3 @@ pub fn test_flatten_err() {
     debug(format!("{:?}", json2).as_str());
     assert_eq!("{\"r\":false,\"d\":null,\"e\":\"CommonError -> InternalErr -> for test\"}".to_string(), json2.unwrap());
 }
-
-#[test]
-fn test_wx_encode_decode() {
-    let session_key = "E9+B5t8W2532yB4O8osEDA==";
-    let iv = "797GckylrepXN+lwKrq7QA==";
-    let encrypted_data = "l69ejoQf+1S1TFx6uJi7m0XqHzygEh9OIjna4lw8SWcgUQ7IVxIMKEFNUSZ4nHb+vDBVeN0E/LFPFkqsf44YhE4yXl9H3eymKDoXzDT0Qalr66zMbN9JU5TlBlP2TKNSfAzl+5Ci36Ysxp3YIrMYT7e3f5G3C4Jzodyn5TPqmnpAlkU202LIZKjFNv4UKUz8kWd9tNuhjDS7HGBfN8hBHn5ixd/y0UtmABJeiMtnKQfsAZh8pL3jTXmBnyn0zkq6Gd6+Mh6LSDZ52NEOSwnjhgRVhLBV9Nhr/hLd3xQ4/rau0idJZbNUVEjT9IJ+RAD695NpkLbHMFNCAK9tSRvdubwM4c/XOiVL+7tl0jd9BfGxJHV3JdeeBNtwnfMdHYIxZkl4podZREDWDgfLbHzllYyvZZdLkFg8p1huxkEGMyzDyEQQX2J51xmKB0htuGhK6wCsfAmuTR5EdKtkM2ORNCepo6pwcKfkQWihc6Dg/wYwy3vNpeFQbMrEEhRSIVM9wE8TnbrLfJLoOaF6YHAsK26U7dUYsAENunMaeCRUlac=";
-
-    let params = WxParams {
-        code: None,
-        appid: None,
-        encrypted_data: Some(encrypted_data.into()),
-        iv: Some(iv.into()),
-        secret: None,
-    };
-
-    let decrypted_data = wx_decode(&params, session_key);
-    assert_eq!("{\"openId\":\"o-dnm5ffBPBP4y8bsRDetmE8Gfco\",\"nickName\":\"木逸辰\",\"gender\":1,\"language\":\"zh_CN\",\"city\":\"Haidian\",\"province\":\"Beijing\",\"country\":\"China\",\"avatarUrl\":\"https://wx.qlogo.cn/mmopen/vi_32/VM3Aa9z5YurfbUXica6LcZNcUnTXPs8UJEMicGXicHXuOfqA4bM5zOVia4OoCY8CpHiaGRAgj1HdtQnlh3zLeZKTRTQ/132\",\"unionId\":\"o8vss0XUO8eynm2cjS9MMm7Obx1g\",\"watermark\":{\"timestamp\":1568131106,\"appid\":\"wx0e30fcb4f4fa808a\"}}",
-               decrypted_data);
-
-    let wx_user = serde_json::from_str::<WxUser>(decrypted_data.as_str());
-    assert_eq!(
-        "o-dnm5ffBPBP4y8bsRDetmE8Gfco",
-        wx_user.unwrap().openId.unwrap()
-    );
-}
